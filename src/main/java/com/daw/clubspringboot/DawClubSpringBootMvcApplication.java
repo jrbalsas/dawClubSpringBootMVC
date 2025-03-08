5b1dde2ee170d96406d08a0b53f7aff148e9e5fb
José Ramón Balsas <jrbalsas@ujaen.es>
2025-03-08 11:57:50 +0100

    Update JDK 21 support

--- src/main/java/com/daw/clubspringboot/DawClubSpringBootMvcApplication.java
+++ src/main/java/com/daw/clubspringboot/DawClubSpringBootMvcApplication.java
@@ -1,9 +1,17 @@
 package com.daw.clubspringboot;
 
+import com.daw.clubspringboot.controller.ClienteController;
+import com.daw.clubspringboot.model.Cliente;
+import com.daw.clubspringboot.model.dao.ClienteDAO;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+
+import java.util.logging.Logger;
 
 /** SpringBoot configuration class for deploying in JEE application server
  *
@@ -12,6 +20,8 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @SpringBootApplication
 public class DawClubSpringBootMvcApplication extends SpringBootServletInitializer {
 
+    private static final Logger logger = Logger.getLogger(ClienteController.class.getName());
+
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(DawClubSpringBootMvcApplication.class);
@@ -19,6 +29,18 @@ public class DawClubSpringBootMvcApplication extends SpringBootServletInitialize
 
     public static void main(String[] args) {
         SpringApplication.run(DawClubSpringBootMvcApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner initApp(@Qualifier("clienteDAOMap") ClienteDAO clienteDAO) {
+        //execute code after the Spring application context has fully started but before the application itself begins running.
+        return (args) -> {
+            logger.info("Creando clientes de prueba");
+
+            clienteDAO.crea( new Cliente(0, "Paco López", "11111111-A", false) );
+            clienteDAO.crea( new Cliente(0, "María Jiménez", "22222222-B", true) );
+            clienteDAO.crea( new Cliente(0, "Carlos García", "33333333-C", true) );
+        };
     }
 
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
 -- Insert sample data for Persistence Unit (important: ONE ROW per operation)
 
-insert into Cliente (dni,nombre,socio,medio_pago) VALUES ('22222222-B','María López',true, 0);
-insert into Cliente (dni,nombre,socio,medio_pago) VALUES ('33333333-C','Samuel Aranda',false, 0);
-insert into Cliente (dni,nombre,socio,medio_pago) VALUES ('44444444-D','Sonia Pérez',false, 0);
-insert into Cliente (dni,nombre,socio,medio_pago) VALUES ('11111111-A','Manuel García',true, 0);
+-- insert into Cliente (dni,nombre,socio,medio_pago) VALUES ('22222222-B','María López',true, 0);
+-- insert into Cliente (dni,nombre,socio,medio_pago) VALUES ('33333333-C','Samuel Aranda',false, 0);
+-- insert into Cliente (dni,nombre,socio,medio_pago) VALUES ('44444444-D','Sonia Pérez',false, 0);
+-- insert into Cliente (dni,nombre,socio,medio_pago) VALUES ('11111111-A','Manuel García',true, 0);
